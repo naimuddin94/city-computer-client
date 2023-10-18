10980773cbaefeee19d86824bbbd0b02c2a38c6e
naimuddin94 <developernaimuddin@gmail.com>
2023-10-19 02:37:42 +0600

    brand page data get from database

--- src/pages/brand-details/BrandDetails.jsx
+++ src/pages/brand-details/BrandDetails.jsx
@@ -6,10 +6,9 @@ const BrandDetails = () => {
   const companies = useLoaderData();
   const { name } = useParams();
   const findBrand = companies.find((company) => company.name === name);
-  console.log(findBrand.advertisement);
 
   useEffect(() => {
-    fetch(`http://localhost:5000/brand/${name}`)
+    fetch(`http://localhost:5000/brands/${name}`)
       .then((res) => res.json())
       .then((data) => console.log(data));
   }, [name]);
