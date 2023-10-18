a9caa91320a0afddd36959a50766e11694b6df73
naimuddin94 <developernaimuddin@gmail.com>
2023-10-18 09:43:30 +0600

    auth provider create

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,88 @@
+import { createContext, useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import {
+  GoogleAuthProvider,
+  GithubAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  sendPasswordResetEmail,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import auth from "../firebase/firebase.config";
+
+export const AuthContext = createContext(null);
+
+const googleProvider = new GoogleAuthProvider();
+const githubProvider = new GithubAuthProvider();
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [darkMode, setDarkMode] = useState(false);
+
+  const switchTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const loginUser = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const signInWithGithub = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+
+  const passwordChange = (email) => {
+    return sendPasswordResetEmail(auth, email);
+  };
+
+  const logOut = () => {
+    signOut(auth);
+  };
+
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    loginUser,
+    signInWithGoogle,
+    signInWithGithub,
+    logOut,
+    passwordChange,
+    setLoading,
+    darkMode,
+    switchTheme,
+  };
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default AuthProvider;
