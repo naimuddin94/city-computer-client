ad9ba829dad785e07d695bc43857f5fa3884a79a
naimuddin94 <developernaimuddin@gmail.com>
2023-10-19 11:35:44 +0600

    product card component design

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "../components/shared/Header";
 import Navbar from "../components/shared/Navbar";
 import useAuthInfo from "../hooks/useAuthInfo";
@@ -6,11 +6,18 @@ import Footer from "../components/shared/Footer";
 
 const MainLayout = () => {
   const { darkMode } = useAuthInfo();
+  const navigation = useNavigation();
   return (
     <div className={darkMode ? "dark bg-gray-800" : "bg-gray-200"}>
       <Header />
       <Navbar />
-      <Outlet />
+      {navigation.state === "loading" ? (
+        <div className="flex items-center justify-center h-[60vh]">
+          <span className="loading loading-bars loading-lg"></span>
+        </div>
+      ) : (
+        <Outlet />
+      )}
       <Footer />
     </div>
   );
--- src/pages/brand-details/BrandDetails.jsx
+++ src/pages/brand-details/BrandDetails.jsx
@@ -1,8 +1,11 @@
 import { useLoaderData, useParams } from "react-router-dom";
 import Sliders from "../../components/shared/Slider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import ProductCard from "../../components/brand-details/ProductCard";
 
 const BrandDetails = () => {
+  const [products, setProducts] = useState([]);
+
   const companies = useLoaderData();
   const { name } = useParams();
   const findBrand = companies.find((company) => company.name === name);
@@ -10,11 +13,26 @@ const BrandDetails = () => {
   useEffect(() => {
     fetch(`http://localhost:5000/brands/${name}`)
       .then((res) => res.json())
-      .then((data) => console.log(data));
+      .then((data) => setProducts(data));
   }, [name]);
+
   return (
     <div>
       <Sliders data={findBrand.advertisement} />
+      <h1 className="text-4xl text-center font-black dark:text-slate-400 py-5">
+        Our Products
+      </h1>
+      {products.length === 0 ? (
+        <div className="flex justify-center items-center h-[50vh]">
+          <h2 className="text-xl font-semibold text-center dark:text-slate-400">No product found</h2>
+        </div>
+      ) : (
+        <div className="flex flex-wrap gap-5 items-stretch justify-center py-8">
+          {products?.map((product) => (
+            <ProductCard key={product._id} product={product} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
